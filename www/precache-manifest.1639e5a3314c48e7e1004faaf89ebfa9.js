dc057598ac0b540dcc2e11be271c17f43af2844b
Oier Barruso <obarruso@pop-os.localdomain>
2023-09-03 17:40:20 +0200

    first commit

---
+++ www/precache-manifest.1639e5a3314c48e7e1004faaf89ebfa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b82557d5438e3226d04b909567f4a61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe21a269da7b0852328b",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "efd688cf2c916def6f07",
+    "url": "./static/js/2.3d1d2612.chunk.js"
+  },
+  {
+    "revision": "0a9e649c333c0ac97b34",
+    "url": "./static/js/3.12174dbd.chunk.js"
+  },
+  {
+    "revision": "fe21a269da7b0852328b",
+    "url": "./static/js/main.bd02f406.chunk.js"
+  },
+  {
+    "revision": "82cc44f5eafe5d5d406e",
+    "url": "./static/js/runtime~main.0f05b7a6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
